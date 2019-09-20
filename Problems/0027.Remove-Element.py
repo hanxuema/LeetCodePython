ac75354d914ffc795bacaf1c001037bc6a99d49a
Xavier Xie <xieyuehao@gmail.com>
2019-09-20 21:44:06 +1000

    0027.Remove-Element.py

---
+++ Problems/0027.Remove-Element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i , last= 0, len(nums)
+        while i <= last:
+            if nums[i] == val:
+                nums[i], nums[last] = nums[last], nums[i]
+                last -=1
+            else:
+                i +=1
+
+        return last + 1
