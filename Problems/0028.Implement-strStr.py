eac30b484d52ac73e1aa175c6ca1e906c7bc5885
Xavier Xie <xieyuehao@gmail.com>
2019-09-21 09:35:11 +1000

    0028.Implement-strStr.py

---
+++ Problems/0028.Implement-strStr.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if len(haystack) == 0 or len(needle) == 0 :
+           return 0
+       
+        for i in range(len(haystack) - len(needle)  +  1):
+           if haystack[i : i + len(needle)] == needle:
+               return i
+        return -1
