8084e30dab97d2bcfed2a7f77871fe8efa82db45
Xavier Xie <xieyuehao@gmail.com>
2019-09-23 22:31:36 +1000

    0069.sqrt-x.py

---
+++ Problems/0069.sqrt-x.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=69 lang=python
+#
+# [69] Sqrt(x)
+#
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 4 :
+            return 1
+        left, right = 1, x // 2
+        while left <= right:
+            mid = left + (right - left ) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
+s = Solution()
+print(s.mySqrt(8))
