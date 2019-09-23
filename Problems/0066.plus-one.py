7dec4565844733639e83bdf8326278762b379208
Xavier Xie <xieyuehao@gmail.com>
2019-09-23 18:23:06 +1000

    0066.plus-one.py

---
+++ Problems/0066.plus-one.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=66 lang=python
+#
+# [66] Plus One
+#
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] = digits[i] + 1
+                return digits
+        digits[0] = 1
+        digits.append(0)
+        return digits
