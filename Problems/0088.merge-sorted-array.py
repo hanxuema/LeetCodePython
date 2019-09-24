770e24e99531fcc55949cf73aac4aa85bbf18369
Xavier Xie <xieyuehao@gmail.com>
2019-09-24 20:19:33 +1000

    modify 0088.merge-sorted-array.py

--- Problems/0088.merge-sorted-array.py
+++ Problems/0088.merge-sorted-array.py
@@ -3,6 +3,8 @@
 #
 # [88] Merge Sorted Array
 #
+
+
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
@@ -11,27 +13,31 @@ class Solution(object):
         :type nums2: List[int]
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
-         # two get pointers for nums1 and nums2
-        p1 = m - 1
-        p2 = n - 1
-        # set pointer for nums1
-        p = m + n - 1
-        
-        # while there are still elements to compare
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] < nums2[p2]:
-                nums1[p] = nums2[p2]
-                p2 -= 1
+        """ 
+        while m  > 0 and n  > 0:
+            if nums1[m - 1] >= nums2[n - 1]:
+                nums1[m-1+n], nums1[m-1] = nums1[m-1], nums1[m-1+n] 
+                m = m -1
             else:
-                nums1[p] =  nums1[p1]
-                p1 -= 1
-            p -= 1
-        
-        # add missing elements from nums2
-        nums1[:p2 + 1] = nums2[:p2 + 1] 
-        
-if __name__ == "__main__":
-    nums1 = [1, 3, 5, 0, 0, 0, 0]
-    nums2 =          [3, 4, 6, 7]
-    print(Solution().merge(nums1, 3, nums2, 4))
+                nums1[m-1+n] = nums2[n-1]
+                n = n - 1
+        if  m  == 0 and n > 0:
+            nums1[:n] = nums2[:n]
+            
+        return nums1
+ 
+        # while m > 0 and n > 0:
+        #     if nums1[m-1] < nums2[n-1]:
+        #         nums1[m-1+n] = nums2[n-1]
+        #         n = n - 1
+        #     else:
+        #         nums1[m-1+n], nums1[m-1] = nums1[m-1], nums1[m-1+n]
+        #         m = m -1
+        # if m == 0 and n>0:
+        #     nums1[:n] = nums2[:n]
+        # return nums1
+
+# if __name__ == "__main__":
+#     nums1 = [2, 3, 5, 0, 0, 0, 0]
+#     nums2 =          [1, 4, 6, 7]
+#     print(Solution().merge(nums1, 3, nums2, 4))
