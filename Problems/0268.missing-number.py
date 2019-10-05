70095e1a208f1f1c2fd04a47f68bd7934c44294e
Xavier Xie <xieyuehao@gmail.com>
2019-10-05 17:47:21 +1000

    add 0268.missing-number.py

---
+++ Problems/0268.missing-number.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=268 lang=python
+#
+# [268] Missing Number
+#
+
+# @lc code=start
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+        return len(nums)
+# @lc code=end
+
