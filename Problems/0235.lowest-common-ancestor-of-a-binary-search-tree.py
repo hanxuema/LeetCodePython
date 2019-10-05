97b2349c5be19978327000e53ccb65a1f02a397b
Xavier Xie <xieyuehao@gmail.com>
2019-10-05 14:48:29 +1000

    add 0235.lowest-common-ancestor-of-a-binary-search-tree.py while

---
+++ Problems/0235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=235 lang=python
+#
+# [235] Lowest Common Ancestor of a Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        pointer = root
+        while pointer:
+            if p.val < pointer.val and q.val < pointer.val:
+                pointer = pointer.left
+            elif p.val > pointer.val and q.val > pointer.val:
+                pointer = pointer.right
+            else:
+                return pointer
+        
+# @lc code=end
+
