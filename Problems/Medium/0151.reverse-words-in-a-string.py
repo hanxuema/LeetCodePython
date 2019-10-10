825ca6a8b5d68727c5b85bcbe81a5d80a0e428ed
Xavier Xie <xieyuehao@gmail.com>
2019-10-10 21:06:00 +1000

    add 0151.reverse-words-in-a-string.py

---
+++ Problems/Medium/0151.reverse-words-in-a-string.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=151 lang=python
+#
+# [151] Reverse Words in a String
+#
+
+# @lc code=start
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return " ".join(s.strip().split()[::-1])
+# @lc code=end
+
