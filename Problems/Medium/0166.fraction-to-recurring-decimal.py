1a4504973044994484644cbc6f9e3879f13189c1
Xavier Xie <xieyuehao@gmail.com>
2019-10-12 13:29:00 +1000

    update 0166.fraction-to-recurring-decimal.py

--- Problems/Medium/0166.fraction-to-recurring-decimal.py
+++ Problems/Medium/0166.fraction-to-recurring-decimal.py
@@ -15,29 +15,32 @@ class Solution(object):
         if numerator == 0 or denominator == 0:
             return "0"
         res = []
-        if ((numerator > 0 and denominator < 0) or (numerator<0 and denominator >0) ):
+        if ((numerator > 0 and denominator < 0 ) or(numerator < 0 and denominator > 0)):
             res.append("-")
-        divisor = abs(numerator) #2
-        dividend = abs(denominator) #3
-        res.append(str(divisor // dividend)) #0
-        remainder = divisor % dividend # 2
+        divisor = abs(numerator)
+        dividend = abs(denominator)
+        remainder = divisor % dividend
+
+        res.append(str(divisor // dividend)) # 2
         if remainder == 0:
-            return "".join(res)
-        res.append(".")
+            return "".join(res) # 6/3 = 2
+
+        res.append(".") # else 8/3 =  2.6666666   2.
         lookup = {}
-        while remainder != 0: 
-            if remainder in lookup:
-                res.insert(lookup[remainder],"(")
+        while remainder != 0:
+            if remainder in lookup:  # lookup has {2:0, .:1, 6:1}
+                res.insert(lookup[remainder], "(")
                 res.append(")")
                 break
-            lookup[remainder] = len(res) #res = [0, . , ] {2:2}
-            remainder *= 10  # 2 * 10 = 30
-            res.append(str(remainder // dividend))  # [0.6]
-            remainder %= dividend
-        
+            else:
+                lookup[remainder] = len(res) # 6
+                remainder = remainder * 10
+                res.append(str(remainder // dividend))  # 2.6
+                remainder = remainder % dividend
+
         return "".join(res)
 
-s = Solution()
-print(s.fractionToDecimal(2,3))
+# s = Solution()
+# print(s.fractionToDecimal(2,3))
 # @lc code=end
 
