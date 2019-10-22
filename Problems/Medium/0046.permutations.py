3fcc451d9015be6fad43d0f08eefa3a45c92ca64
xiexav01 <xavier.xie@csgi.com>
2019-10-22 16:26:28 +1000

    add 0046.permutations.py

--- Problems/Medium/0046.permutations.py
+++ Problems/Medium/0046.permutations.py
@@ -7,21 +7,24 @@
 # @lc code=start
 class Solution(object):
     def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        if len(nums) == 1:
-            return [nums]
+        if nums is None or len(nums) == 0:
+            return []
         res = []
-        for i, num in enumerate(nums):
-            n = nums[:i] + nums[i+1:]
-            for y in self.permute(n):
-                res.append([num]+y)
+        self.dfs(nums, res, [])
         return res
-
+    
+    def dfs(self, nums, res, subset):
+        if len(nums) == len(subset):
+            res.append(subset)
+            return
+        for i in range(len(nums)):
+            if nums[i] in subset:
+                continue
+            ss = subset + [nums[i]]
+            self.dfs(nums,res,ss)
 
 # s = Solution()
 # s.permute([1,2,3])
 # @lc code=end
-
+s = Solution()
+print(s.permute([1,2,3]))
